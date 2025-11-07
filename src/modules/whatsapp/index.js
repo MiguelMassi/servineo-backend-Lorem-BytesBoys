4b05b3f768789826e21b827e0bff581e818fa8b0
Miguel <miguelangelmassigeronimo@gmail.com>
2025-11-07 08:13:27 -0400

    refactor: Integracion de configuracion centralizada en servicios

---
+++ src/modules/whatsapp/index.js
@@ -0,0 +1,7 @@
+const WhatsAppService = require('./whatsAppService');
+const WhatsAppValidatorService = require('./whatsAppValidatorService');
+
+module.exports = {
+  WhatsAppService,
+  WhatsAppValidatorService
+};
